e063c1ae28004e335573b698166b13c1f9a71387
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-07-08 12:34:20 +0300

    feat : add vercel json

--- src/app/components/TotalBalanceBox.tsx
+++ src/app/components/TotalBalanceBox.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 import DoughnutChat from './DoughnutChat'
-import AnimatedCount from './animatedCount'
+import AnimatedCount from './AnimatedCount'
 
 
 const TotalBalanceBox = ({accounts, totalBanks, totalCurrentBalance}: TotalBalanceBoxProps) => {
