0301795309c234f335b85fdbe7b89832096092bb
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-07-08 12:52:51 +0300

    modify header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Header = ({applyMedia=false}:Boolean) => {
+const Header = ({applyMedia=false}:any) => {
     return (
         <>
             {/*start header */}
